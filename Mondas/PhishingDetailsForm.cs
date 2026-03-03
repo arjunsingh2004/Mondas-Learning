4eb822acd881f4220849f2afbc2f8f164fb74577
Arjun Singh <220042769@aston.ac.uk>
2026-03-03 23:13:18 +0000

    Design for PhishingDetailsForm complete

---
+++ Mondas/PhishingDetailsForm.cs
@@ -0,0 +1,41 @@
+﻿using Syncfusion.WinForms.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mondas
+{
+    public partial class PhishingDetailsForm : SfForm
+    {
+        public PhishingDetailsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void PhishingDetailsForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCopySummary_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCopyEmail_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
