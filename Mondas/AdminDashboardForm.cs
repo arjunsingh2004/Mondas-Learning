df112ba0e35ad7915fb2927fd6de2121d42315ff
asingh2004 <220042769@aston.ac.uk>
2026-03-23 23:05:35 +0000

    Design for admin dashboard added

---
+++ Mondas/AdminDashboardForm.cs
@@ -0,0 +1,31 @@
+﻿using Syncfusion.WinForms.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mondas
+{
+    public partial class AdminDashboardForm : SfForm
+    {
+        public AdminDashboardForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AdminDashboardForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnlAvgAccuracy_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
--- Mondas/Program.cs
+++ Mondas/Program.cs
@@ -57,7 +57,7 @@ namespace Mondas
 
             _host.Start();
 
-            Application.Run(new LoginForm());
+            Application.Run(new AdminDashboardForm());
 
             _host.StopAsync().GetAwaiter().GetResult();
             _host.Dispose();
